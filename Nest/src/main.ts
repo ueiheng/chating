5a6578809d28c9ffa57c120787a28e065f91546d
Asen <1919019774@qq.com>
2023-09-17 12:00:40 +0800

    init Next

--- Nest/src/main.ts
+++ Nest/src/main.ts
@@ -6,7 +6,7 @@ declare const module: any;
 // 使用核心函数 NestFactory 创建 Nest 应用实例的应用入口文件。
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  await app.listen(3000);
+  await app.listen(3001);
   if (module.hot) {
     module.hot.accept();
     module.hot.dispose(() => app.close());
